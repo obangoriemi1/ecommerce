6497186aea8a674a931c411c12772ce44c9f1060
kuchobang <obangoriemi1@gmail.com>
2025-01-12 17:53:52 -0600

    complete navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Login from './pages/Login'
+import Card from './pages/Card'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Navbar from './componnents/Navbar'
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+       <Navbar/>
+      <Routes>
+       
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/collection' element={<Collection/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/card' element={<Card/>}></Route>
+        <Route path='/orders' element={<Orders/>}></Route>
+        <Route path='/place-order' element={<PlaceOrder/>}></Route>
+        <Route path='/product/:productId' element={<Product/>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
