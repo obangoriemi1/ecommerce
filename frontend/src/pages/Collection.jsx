46334687b6b2309fe6c133a7a7baefded365913f
kuchobang <obangoriemi1@gmail.com>
2025-01-16 12:47:29 -0600

    added sort functionality

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -11,6 +11,7 @@ const Collection = () => {
     const [filterProducts, setFilterProducts] = useState([])
     const [category, setCategory] = useState([])
     const [subCategory, setSubCategory] = useState([])
+    const [sortType, setSortType] = useState('relavent')
        
      const toggleCategory = (e) =>{
         if(category.includes(e.target.value)){
@@ -42,11 +43,29 @@ const Collection = () => {
         setFilterProducts(productsCopy)
      }
 
-    
+    const sortProduct = () =>{
+        let fpCopy = filterProducts.slice();
+
+        switch(sortType){
+            case 'low-high':
+                setFilterProducts(fpCopy.sort((a,b) =>(a.price - b.price )))
+                break;
+            case 'high-low':
+                setFilterProducts(fpCopy.sort((a,b) =>(b.price - a.price ))) 
+                break;
+            default:
+                applyFilter();
+                break;
+        }
+    }
 
   useEffect(() =>{
     applyFilter()
   },[category, subCategory])
+
+  useEffect(() =>{
+         sortProduct();
+  },[sortType])
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t '>
       {/* filter options */}
@@ -92,7 +111,7 @@ const Collection = () => {
          <div className="flex justify-between text-base sm:text-2xl mb-4 ">
             <Title text1={"All"} text2={"COLLECTIONS"}/>
             {/* product sort */}
-            <select className='border border-gray-300 text-sm px-2 '>
+            <select onChange={(e) =>setSortType(e.target.value)} className='border border-gray-300 text-sm px-2 '>
                <option value="relevent">Sort by: Relevent</option>
                <option value="low-high">Sort by: Low to High</option>
                <option value="high-low">Sort by: High to Low</option>
